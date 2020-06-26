4a41f32d365b46bcc8b19078c24a1e8455efd3d0
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-06-26 09:50:59 +0100

    WIP: retrieve query params from FormValue

--- handlers/register.go
+++ handlers/register.go
@@ -39,7 +39,7 @@ func Register(mainRouter *mux.Router, cfg *config.Config, authCodeDao dao.Authco
 	appRouter.Use(userAuthInterceptor.UserAuthenticationIntercept)
 
 	// Declare endpoint URIs
-	appRouter.HandleFunc("/company/{company_number}/officers", GetCompanyOfficers).Queries("start_index", "{start_index:([0-9]+)?}", "items_per_page", "{items_per_page:([0-9]+)?}").Methods(http.MethodGet).Name("get-company-officers")
+	appRouter.HandleFunc("/company/{company_number}/officers", GetCompanyOfficers).Methods(http.MethodGet).Name("get-company-officers")
 	appRouter.HandleFunc("/company/{company_number}/officers/{officer_id}", GetCompanyOfficer).Methods(http.MethodGet).Name("get-company-officer")
 	appRouter.Handle("/auth-code-requests", CreateAuthCodeRequest(authCodeRequestService)).Methods(http.MethodPost).Name("create-auth-code-request")
 	appRouter.Handle("/auth-code-requests/{auth_code_request_id}", GetAuthCodeRequest(authCodeRequestService)).Methods(http.MethodGet).Name("get-auth-code-request")
