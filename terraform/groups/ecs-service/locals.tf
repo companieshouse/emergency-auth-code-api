7f83b6ff1fafb51b149613172ff01187d3b663f4
hreddy-ch <hreddy@companieshouse.gov.uk>
2025-03-28 11:44:21 +0000

    Update listener paths and healthcheck for ECS service (#58)

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,8 +9,8 @@ locals {
   docker_repo                 = "emergency-auth-code-api"
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority   = 35
-  lb_listener_paths           = ["/emergency-auth-code", "/emergency-auth-code-bulk"]
-  healthcheck_path            = "/healthcheck"
+  lb_listener_paths           = ["/emergency-auth-code-service/*"]
+  healthcheck_path            = "/emergency-auth-code-service/healthcheck"
   healthcheck_matcher         = "200"
   vpc_name                    = local.stack_secrets["vpc_name"]
   s3_config_bucket            = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
