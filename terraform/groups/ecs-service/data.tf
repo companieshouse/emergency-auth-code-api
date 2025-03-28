3d33e0c20be8be5a63c34d0cf6c4347c12011db9
hreddy-ch <hreddy@companieshouse.gov.uk>
2025-03-28 11:04:19 +0000

    Update load balancer name to include environment variable (#57)

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -34,7 +34,7 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "service_lb" {
-  name = "emergency-auth-code"
+  name = "${var.environment}-chs-chgovuk"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
