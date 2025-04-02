922fb20f72475fed83733ffb06dc24f77ce6788d
Mike Kelly <mkelly1@companieshouse.gov.uk>
2025-04-02 13:29:07 +0100

    test request data.

--- models/request_entities.go
+++ models/request_entities.go
@@ -5,6 +5,7 @@ import "github.com/companieshouse/chs.go/authentication"
 // AuthCodeRequest is the data received when creating a new Auth Code Request.
 type AuthCodeRequest struct {
 	CompanyNumber   string                         `json:"company_number" validate:"required"`
+	CompanyName     string                         `json:"company_name" validate:"required"`
 	CreatedBy       authentication.AuthUserDetails `json:",omitempty" validate:"required"`
 	OfficerID       string                         `json:"officer_id" validate:"required"`
 	Status          string                         `json:"status"`
