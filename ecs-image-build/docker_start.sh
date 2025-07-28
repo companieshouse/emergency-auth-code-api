75a90f4ded883e3bc7a505c09088118480e90e20
Dan Toulcher <dtoulcher@companieshouse.gov.uk>
2025-07-28 07:58:26 +0100

    [cc-2725] log the Kafka brokers being passed into the service

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -6,6 +6,9 @@ PORT="20188"
 # Read brokers and topics from environment and split on comma
 IFS=',' read -ra BROKERS <<< "${KAFKA_BROKER_ADDR}"
 
+echo Kafka broker addresses: "${KAFKA_BROKER_ADDR}"
+echo "Kafka brokers: ${BROKERS[@]}"
+
 # Ensure we only populate the broker address and topic via application arguments
 unset KAFKA_BROKER_ADDR
 
